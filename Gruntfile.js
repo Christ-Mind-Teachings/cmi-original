159d56132cb0ad5f5c531652d06c9745f03d5068
Rick Mercer <rmercer33@gmail.com>
2017-01-31 15:31:55 +0800

    added bourbon/refills components

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,24 +17,24 @@ module.exports = function(grunt) {
       options: {
         accessKeyId: "<%= aws.AWSAccessKeyId %>", // Use the variables
         secretAccessKey: "<%= aws.AWSSecretKey %>", // You can also use env variables
-        region: "ap-southeast-1",
+        region: "us-east-1",
         uploadConcurrency: 5,
         downloadConcurrency: 5
       },
-      prod: {
+      nwffacim: {
         options: {
-          bucket: "christmind.info",
+          bucket: "assets.christmind.info",
           differential: true,
           gzipRename: "ext"
         },
         files: [
-          {action: "upload", expand: true, cwd: "./_site", src: ["**"], dest: "/"}
+          {action: "upload", expand: true, cwd: "./src/aws/nwffacim/books", src: ["**"], dest: "/nwffacim/books"}
         ]
       }
     }
   });
 
-  grunt.registerTask("deploy-prod",  ["aws_s3:prod"]);
+  grunt.registerTask("aws-nwffacim",  ["aws_s3:nwffacim"]);
 
 };
 
--- _nwffacim/yaa/yaa.md
+++ _nwffacim/yaa/yaa.md
@@ -1,15 +1,18 @@
 ---
-layout: introduction
+layout: hero
 title: Introduction
 book: You Are the Answer, A Journey of Awakening
 source: Northwest Foundation for ACIM
 surl: /nwffacim/
 ---
-
 {% comment %}
-class: grad
-contents: nwffacim/yaa/contents.html
+This is the {{page.book}} page.
 {% endcomment %}
 
-This is the {{page.book}} page.
+<div class="hero">
+  <div class="hero-content">
+    <img src="/public/img/nwffacim/yaa/cover-s.jpg" alt="Logo Image" class="hero-logo">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni modi doloremque excepturi laudantium maxime explicabo cumque deleniti voluptate deserunt.</p>
+  </div>
+</div>
 
